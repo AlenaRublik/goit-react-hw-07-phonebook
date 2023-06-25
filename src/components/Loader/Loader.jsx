88a93e3bf4017c0a7246d1a3ea4741cf06371cb4
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2023-06-25 20:26:49 +0300

    change coordinatec

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,19 +6,19 @@ export const Loader = props => {
   return (
     <ContentLoader
     speed={2}
-    width={600}
+    width={1200}
     height={160}
-    viewBox="0 0 600 160"
+    viewBox="0 0 1200 160"
       backgroundColor="#8a9bff"
     foregroundColor="#ecebeb"
       {...props}
     >
-      <rect x="120" y="6" rx="4" ry="4" width="450" height="38" />
-      <rect x="80" y="6" rx="4" ry="4" width="35" height="38" />
-      <rect x="120" y="55" rx="4" ry="4" width="450" height="38" />
-      <rect x="80" y="55" rx="4" ry="4" width="35" height="38" />
-      <rect x="120" y="104" rx="4" ry="4" width="450" height="38" />
-      <rect x="80" y="104" rx="4" ry="4" width="35" height="38" />
+      <rect x="520" y="6" rx="4" ry="4" width="450" height="38" />
+      <rect x="480" y="6" rx="4" ry="4" width="35" height="38" />
+      <rect x="520" y="55" rx="4" ry="4" width="450" height="38" />
+      <rect x="480" y="55" rx="4" ry="4" width="35" height="38" />
+      <rect x="520" y="104" rx="4" ry="4" width="450" height="38" />
+      <rect x="480" y="104" rx="4" ry="4" width="35" height="38" />
 
     </ContentLoader>
   )
